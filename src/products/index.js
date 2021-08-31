4be8e2e0e9302661659ffd9aff93fbe487d352a2
Rabz92 <rabbiailyas92@gmail.com>
2021-08-31 15:59:21 +0200

    finished testing delete/put requests

--- src/products/index.js
+++ src/products/index.js
@@ -11,7 +11,12 @@ productsRouter.get('/', async (req, res) => {
 productsRouter.get('/:id', async (req, res) => {
     try {
         const product = await ProductModel.findById(req.params.id)
-        res.status(200).send(product)
+        if(product){
+            res.status(200).send(product)
+        }
+        else{
+            res.status(404).send(`Product with id: ${req.params.id} not found`)
+        }
     } catch (error) {
         res.status(404).send()
     }
@@ -28,6 +33,35 @@ productsRouter.post('/', async (req, res) => {
         res.status(400).send()
     }
 
+})
+
+productsRouter.put('/:id', async (req, res) => {
+    try {
+        const updatedProduct = await ProductModel.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if(updatedProduct){
+            res.send(updatedProduct)
+        }else{
+            res.status(404).send(`Product with id: ${req.params.id} not found`)
+        }
+    } catch (error) {
+        res.status(404).send()
+    }
+})
+
+productsRouter.delete('/:id', async (req, res) => {
+    try {
+        const product = await ProductModel.findByIdAndDelete(req.params.id)
+        if(product){
+            res.status(204).send()
+        }else{
+            res.status(404).send(`Product with id: ${req.params.id} not found`)
+        }
+    } catch (error) {
+        res.status(404).send()
+    }
 })
 
 
